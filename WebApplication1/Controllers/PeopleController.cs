a4d0eed649cfe67780506007390397d2c15a1eba
ethanhousley <66891702+ethanhousley@users.noreply.github.com>
2021-06-17 15:55:08 -0500

    Add project files.

---
+++ WebApplication1/Controllers/PeopleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            var Ethan = new Person();
+            
+
+            return View(Ethan);
+
+        }
+        
+        
+        public IActionResult AddPerson()
+        {
+            
+           
+            return View();
+
+        }
+        
+        [HttpPost]
+        public IActionResult NewUser(Person person)
+        {
+
+            var newUser = person;
+            
+            
+            return View(newUser);
+
+        }
+    }
+}
---
+++ WebApplication1/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        
+        
+        public int UserID { get; set; } 
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+    }
+}
